2aa3da269085dde6229d5fa0544c2749642b2624
EndijsV03 <93904432+EndijsV03@users.noreply.github.com>
2023-11-05 19:22:13 +0200

    Update P_uzd.cpp

--- 4_nodarbibas_uzd/P_uzd.cpp
+++ 4_nodarbibas_uzd/P_uzd.cpp
@@ -3,20 +3,34 @@
 
 using namespace std;
 
-void Backword(char data[]){
-    for (int i = strlen(data); i >= 0; i--){
-        cout << data[i];
+void Backword(char data[], int n){
+    int start = 0;
+    int end = n - 1;
+
+    while (start < end){
+        char temp = data[start];
+        data[start] = data[end];
+        data[end] = temp;
+
+        start++;
+        end--;
     }
 } 
 
 int main()
 {
+    int length;
     char text[100];
 
     cout << "Ievadiet simbolu virkni: ";
     cin.get(text, 100);
 
-    Backword(text);
+    length = strlen(text);
+
+    Backword(text, length);
+    for(char i: text){
+        cout << i;
+    }
 
     return 0;
     
