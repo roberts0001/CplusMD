96e2ea27b0d4dc21c3061601a522bf94a6f44d5b
EndijsV03 <93904432+EndijsV03@users.noreply.github.com>
2023-11-12 21:13:22 +0200

    Add files via upload

---
+++ 5_praktiskais_darbs/Praktiskais_darbs.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+struct InventoryItem { //structure for items
+    string name;
+    double price;
+    int quantity;
+    int sold_quantity;
+    bool saved;
+};
+
+class InventoryManager {
+    verctor<InventoryItem> inventory; //place to save items 
+
+    void addItem() { //endijs
+        
+    }
+
+    void saveItem() { //endijs
+
+    } 
+
+    void displayItems() { //endijs
+
+    }
+
+    void sellItem() { //roberts
+
+    }
+
+    void searchItem() { //roberts
+
+    }
+
+}
+
+int main() {
+    InventoryManager inventorymanager;
+    int choice;
+
+    while(true) { // izvelne Roberts (switch)
+        
+
+
+    }
+    return 0;
+}
